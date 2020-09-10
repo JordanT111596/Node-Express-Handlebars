72d19699c4f21a221383aacde42f3e5086372eaf
Jordan Triplett <JordanT111596@gmail.com>
2020-09-09 21:24:45 -0400

    Starter files with name changes added

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Luther Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Impossible Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('In-N-Out Burger', true);
