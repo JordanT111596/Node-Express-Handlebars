62b1d13647815e48f2f949382c2f704622b4df7d
Jordan Triplett <JordanT111596@gmail.com>
2020-09-10 17:59:10 -0400

    Everything added besides good looking css

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,5 @@ USE burgers_db;
 INSERT INTO burgers (burger_name, devoured) VALUES ('The Luther Burger', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('The Impossible Burger', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('In-N-Out Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Strip Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -27,7 +27,7 @@ $(function() {
 
     var newBurger = {
       burger_name: $("#ca").val().trim(),
-      devoured: $("[burger_name=devoured]:checked").val().trim()
+      devoured: false
     };
 
     // Send the POST request.
